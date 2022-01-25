8b950d7a099d04fadf997dd38f2e6e41db0bc74c
theThorsager <kallethorsager@gmail.com>
2022-01-25 11:12:21 +0100

    Move Root of Repo

---
+++ ACEX30/ACEX30Info.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace ACEX30
+{
+    public class ACEX30Info : GH_AssemblyInfo
+    {
+        public override string Name => "ACEX30";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("D91DD523-6DD3-4A1C-BA77-D86AB326CE0B");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
