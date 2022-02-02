241ee54fc4be7aeac310dd34f9f6bcd901f72c9a
Tanggorn <98388510+Tanggorn@users.noreply.github.com>
2022-02-02 15:14:39 +0100

    Update BBperi
    Now uses Voxels

--- Krill/BBperi.cs
+++ Krill/BBperi.cs
@@ -10,59 +10,92 @@ namespace Krill
     internal class BBperi
     {
         public double bond_stiffness;
-        public Point3d[] startPos;
-        public Point3d[] u;     // Current displacements
-        public int[,] nlist;    // Neighbour list
-        public double[] vol;   // Volumes of the particles
-        public Vector3d[] force;
-        public int[,] fbc_types;    // Boundary force type
-        public Vector3d[] fbc;      // Boundary force
-        public double fbc_scale;
-        public List<Vector3d> body_force = new List<Vector3d>();
+        public Voxels<int> startVoxels;
+        public int[] nlist;    // Neighbour list
+        public double vol;   // Volume of the particles (same for all)
+        public double dt;   // Time step
 
-        public BBperi(Point3d[] OrgPos, Point3d[] currentDisp, double Bond_stiffness, int[,] neighbour_list, double[] volumes, Vector3d[] Force, int[,] ForceBC_type, Vector3d[] ForceBC, double ForceBC_scale)
+        public Voxels<Vector3d> forceVoxels;
+        public Voxels<Vector3d> dispVoxels;
+
+        private int noVoxels;
+
+        public BBperi(Voxels<int> orgVoxels, double Bond_stiffness, int[] neighbour_list,
+            double volume, double time_step)
         {
-            startPos = OrgPos;
-            u = currentDisp;
+            startVoxels = orgVoxels;
             bond_stiffness = Bond_stiffness;
             nlist = neighbour_list;
-            vol = volumes;
-            force = Force;
-            fbc_types = ForceBC_type;   // Behövs den här just nu?
-            fbc = ForceBC;
-            fbc_scale = ForceBC_scale;
+            vol = volume;
+            dt = time_step;
 
-            Vector3d[] local_cache = new Vector3d[startPos.Length];
+            noVoxels = startVoxels.n;
+            forceVoxels = new Voxels<Vector3d>(startVoxels.origin, startVoxels.delta, noVoxels);
+            dispVoxels = new Voxels<Vector3d>(startVoxels.origin, startVoxels.delta, noVoxels);         
+
+        }
+
+        public void UpdateForce()
+        {
+
+            double[] local_cache_x = new double[noVoxels];
+            double[] local_cache_y = new double[noVoxels];
+            double[] local_cache_z = new double[noVoxels];
+
+            int noBonds = nlist.Length;
 
             // Assuming the bonds never break, otherwise need a new if-statement
-            for (int i = 0; i < startPos.Length; i++)
+            for (int i = 0; i < noVoxels*noVoxels*noVoxels; i++)
             {
-                int noBonds = nlist.GetLength(i);   // Number of bonds for that particle
-
-                for (int j = 0; j < noBonds; j++)
+                if (startVoxels.cellValues[i] == 0)
                 {
-                    Vector3d xi_vec;
-                    Vector3d xi_eta;
+                    continue;
+                }
 
-                    xi_vec = startPos[j] - startPos[i];
-                    xi_eta = u[j] - u[i] + xi_vec;
+                forceVoxels.cellValues[i] = Vector3d.Zero;
+
+                for (int a = 0; a < noBonds; a++)
+                {
+
+                    int j = i + nlist[a];
+
+                    if (startVoxels.cellValues[j] == 0)
+                    {
+                        continue;
+                    }
+
+                    Vector3d xi_vec = startVoxels.IndexToPoint(j)-startVoxels.IndexToPoint(i);
+
+                    Vector3d xi_eta_vec = dispVoxels.cellValues[j] - dispVoxels.cellValues[i] + xi_vec;
 
                     double xi = xi_vec.Length;
-                    double y = xi_eta.Length;
+                    double y = xi_eta_vec.Length;
                     double s = (y - xi) / xi;
 
-                    double f = s * bond_stiffness * vol[j];
-                    local_cache[i] += f * xi_eta / y;    // Node force (?)
+                    double f = s * bond_stiffness * vol;
+
+                    forceVoxels.cellValues[i] += f * xi_eta_vec / y;
 
                 }
 
-                body_force.Add(local_cache[i]);
+            }
+        }
 
-                force[i] = body_force[i] + fbc_scale * fbc[i];
+        public void UpdateDisp()
+        {
 
+            for (int i = 0; i < noVoxels; i++)
+            {
+                if (startVoxels.cellValues[i] == 0)
+                {
+                    continue;
+                }
+
+                dispVoxels.cellValues[i] += dt * forceVoxels.cellValues[i];
             }
 
         }
+
 
     }
 }
