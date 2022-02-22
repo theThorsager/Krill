d328870c5f64ca87f1da8aee663591423ff235c2
theThorsager <kallethorsager@gmail.com>
2022-02-22 15:52:02 +0100

    Truss saker

---
+++ Krill/Grasshopper/Truss.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Krill.Grasshopper
+{
+    public class Truss : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the Truss class.
+        /// </summary>
+        public Truss()
+          : base("Truss", "Truss",
+              "Description",
+              "Krill", "TestComponents")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("pts", "pts", "", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("a", "a", "", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("b", "b", "", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("locked", "locked", "", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddLineParameter("lines", "l", "", GH_ParamAccess.list);
+            pManager.AddNumberParameter("values", "vals", "", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var nodes = new List<Point3d>();
+            var a = new List<int>();
+            var b = new List<int>();
+            var locked = new List<bool>();
+            DA.GetDataList(0, nodes);
+            DA.GetDataList(1, a);
+            DA.GetDataList(2, b);
+            DA.GetDataList(3, locked);
+
+            Krill.Truss truss = new Krill.Truss(nodes, locked, a, b);
+
+            //truss.model.Solve();
+            truss.model.Solve_MPC();
+
+
+            DA.SetDataList(0, truss.ToLines());
+            DA.SetDataList(1, truss.GetAxialForces());
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("237B1730-D267-4C97-BFA0-4F06A5173753"); }
+        }
+    }
+}
---
+++ Krill/Truss.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+using BriefFiniteElementNet;
+using BriefFiniteElementNet.Elements;
+using BriefFiniteElementNet.Sections;
+using BriefFiniteElementNet.Materials;
+
+namespace Krill
+{
+    public class Connection
+    {
+        public int IdA;
+        public int IdB;
+        public double Stiffness;
+        public double Load;
+
+        public Connection(int idA, int idB, double stiffness = 1.0)
+        {
+            IdA = idA;
+            IdB = idB;
+            Stiffness = stiffness;
+            Load = 0.0;
+        }
+    }
+
+    public class Truss
+    {
+        // Do we need the voxels here
+        List<Point3d> nodes = new List<Point3d>();
+        List<Connection> connections = new List<Connection>();
+
+        public Model model;
+
+        public Truss(List<Point3d> nodes, List<bool> locked, List<int> startNode, List<int> endNode)
+        {
+            double A = 0.01;
+            double w = Math.Sqrt(A);
+            double E = 1;
+
+            model = new Model();
+
+            for (int i = 0; i < nodes.Count; i++)
+                model.Nodes.Add(new Node(nodes[i].X, nodes[i].Y, nodes[i].Z)
+                { Constraints = locked[i] ? Constraints.MovementFixed : Constraints.Released, Label = $"node{i}" }); 
+
+            UniformGeometric1DSection section = new UniformGeometric1DSection(SectionGenerator.GetRectangularSection(w, w));
+            UniformIsotropicMaterial material = UniformIsotropicMaterial.CreateFromYoungPoisson(E, 0.25);
+
+            for (int i = 0; i < startNode.Count; ++i)
+                model.Elements.Add(new BarElement(model.Nodes[startNode[i]], model.Nodes[endNode[i]])
+                { Behavior = BarElementBehaviours.FullFrame, Section = section, Material = material, Label = $"bar{i}" });
+
+            for (int i = 0; i < nodes.Count; ++i)
+                model.Nodes[i].Loads.Add(new NodalLoad(new Force(0, 0, -1, 0, 0, 0)));
+
+            
+            //model.Nodes[2].Loads.Add(new NodalLoad(new Force(0, 0, -1, 0, 0, 0)));
+            //model.Nodes[3].Loads.Add(new NodalLoad(new Force(0, 0, -1, 0, 0, 0)));
+            //model.Nodes[4].Loads.Add(new NodalLoad(new Force(0, 0, -1, 0, 0, 0)));
+
+            // 0 2 3 4
+            //model.Nodes[0].Settlements.Add(new Settlement(new Displacement(0, 0, -0.1)));
+            //model.Nodes[2].Settlements.Add(new Settlement(new Displacement(0, 0, -0.1)));
+            //model.Nodes[3].Settlements.Add(new Settlement(new Displacement(0, 0, -0.1)));
+            //model.Nodes[4].Settlements.Add(new Settlement(new Displacement(0, 0, -0.1)));
+
+        }
+
+        public List<Line> ToLines()
+        {
+            List<Line> lines = new List<Line>();
+            
+            foreach (Element element in model.Elements)
+            {
+                var a = element.Nodes[0].Location;
+                var b = element.Nodes[1].Location;
+                lines.Add(new Line(new Point3d(a.X, a.Y, a.Z), new Point3d(b.X, b.Y, b.Z)));
+            }
+            return lines;
+        }
+
+        public List<double> GetAxialForces()
+        {
+            var axial = new List<double>();
+            foreach (Element element in model.Elements)
+            {
+                axial.Add((element as BarElement).GetInternalForceAt(0.5).Fx);
+            }
+            return axial;
+        }
+
+        void ComputeForces()
+        {
+            var model = new Model();
+
+            var FEMNodes = new List<Node>();
+            foreach (var node in nodes)
+                FEMNodes.Add(new Node(node.X, node.Y, node.Z) { Constraints = Constraints.RotationFixed });
+
+            var FEMbars = new List<BarElement>();
+            foreach (var connection in connections)
+                FEMbars.Add(new BarElement(FEMNodes[connection.IdA], FEMNodes[connection.IdB]) { Behavior = BarElementBehaviours.Truss });
+
+            model.Nodes.AddRange(FEMNodes);
+            foreach (var bar in FEMbars)
+                model.Elements.Add(bar);
+
+            // Find more supports
+            model.Nodes[0].Settlements.Add(new Settlement(new Displacement(0, 0, -0.01)));
+
+            model.Solve();
+
+            for (int i = 0; i < FEMbars.Count; i++)
+                connections[i].Load = FEMbars[i].GetInternalForceAt(0.5).Fx;
+
+
+        }
+
+        public void ComputeStiffness(Voxels<int> voxels)
+        {
+            // check how much space is available for each connection depending on tensile or compressive load
+
+            // transform to cylindrical coordinates
+            // iterate outwards until the closest point radially, within the height, that is not valid for loads
+
+            // Use that to set the stiffness
+        }
+
+        public bool StaticallyIndeterminate()
+        {
+            return false;
+        }
+    }
+}
