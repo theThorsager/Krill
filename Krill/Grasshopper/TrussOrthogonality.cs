c53c68b1173e3e1b85aac2dc300592af43da1027
theThorsager <kallethorsager@gmail.com>
2022-04-04 09:57:12 +0200

    Truss Optimization Based on Orthogonality

---
+++ Krill/Grasshopper/TrussOrthogonality.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Krill.Grasshopper
+{
+    public class TrussOrthogonality : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the TrussGradient class.
+        /// </summary>
+        public TrussOrthogonality()
+          : base("TrussOrthogonality", "TrussOrtho",
+              "Description",
+              "Krill", "TestComponents")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new Param.TrussGeometryParam());
+            pManager.AddIntegerParameter("n", "n", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("alpha", "a", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("factor", "f", "", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("energy", "e", "", GH_ParamAccess.item);
+            pManager.AddVectorParameter("gradient", "g", "", GH_ParamAccess.list);
+            pManager.AddVectorParameter("disp", "d", "", GH_ParamAccess.list);
+            pManager.AddNumberParameter("strains", "s", "", GH_ParamAccess.list);
+            pManager.AddLineParameter("lines", "l", "", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Param.TrussGeometryGoo truss = null;
+            DA.GetData(0, ref truss);
+
+            int n = 1;
+            DA.GetData(1, ref n);
+
+            double a = 1;
+            DA.GetData(2, ref a);
+
+            double f = 1;
+            DA.GetData(3, ref f);
+
+            if (truss?.Value is null)
+                return;
+
+            var ortho = new OrthonogalityTruss();
+
+            ortho.nodes = truss.Value.Nodes.ToList();
+            ortho.connections = truss.Value.Connections.ToList();
+            ortho.gradients = truss.Value.Nodes.Select(x => new Vector3d()).ToList();
+
+
+            var locked = new bool[ortho.nodes.Count];
+            var indecies = truss.Value.Connections.SelectMany(x => new int[] { x.Item1, x.Item2 }).ToList();
+            for (int i = 0; i < ortho.nodes.Count; i++)
+            {
+                if (indecies.Count(x => x == i) == 1)
+                {
+                    locked[i] = true;
+                }
+            }
+            ortho.LockDOFs(locked.ToList());
+
+
+            double result = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                result = ortho.EvaluateTruss(f);
+                ortho.SetGradients(f);
+                ortho.ApplyGradient(a);
+            }
+
+            //var displac = energyTruss.us;
+
+            //var grad = new List<Vector3d>();
+            //var disp = new List<Vector3d>();
+            //var pts = new List<Point3d>();
+            //for (int i = 0; i < energyTruss.nVariables / 3; i++)
+            //{
+            //    grad.Add(new Vector3d(gradient[i * 3], gradient[i * 3 + 1], gradient[i * 3 + 2]));
+            //    disp.Add(new Vector3d(displac[i * 3], displac[i * 3 + 1], displac[i * 3 + 2]));
+            //    pts.Add(new Point3d(energyTruss.xs[i * 3], energyTruss.xs[i * 3 + 1], energyTruss.xs[i * 3 + 2]));
+            //}
+
+            var lines = new List<Line>();
+            for (int i = 0; i < ortho.connections.Count; i++)
+            {
+                lines.Add(new Line(ortho.nodes[ortho.connections[i].Item1], ortho.nodes[ortho.connections[i].Item2]));
+            }
+
+            //DA.SetData(0, Math.Log(result + f * f));
+            DA.SetData(0, result);
+            DA.SetDataList(1, ortho.gradients);
+            //DA.SetDataList(2, disp);
+            //DA.SetDataList(3, energyTruss.eps);
+            DA.SetDataList(4, lines);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("6D2B14CE-82EE-4AE6-ACF6-3A0E234589E7"); }
+        }
+    }
+}
---
+++ Krill/OrthonogalityTruss.cs
@@ -0,0 +1,138 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krill
+{
+    internal class OrthonogalityTruss
+    {
+        public List<Point3d> nodes = new List<Point3d>();
+        public List<Vector3d> gradients = new List<Vector3d>();
+        public List<Tuple<int, int>> connections = new List<Tuple<int, int>>();
+
+        public List<bool> lockedDOF = new List<bool>();
+
+        public void LockDOFs(List<bool> locked)
+        {
+            lockedDOF = locked.ToList();
+        }
+
+        public double EvaluateTruss(double cutoff)
+        {
+            double sum = 0;
+            for (int i = 0; i < connections.Count; i++)
+            {
+                sum += EvaluateElement(i, cutoff);
+            }
+            return sum;
+        }
+
+        public void SetGradients(double cutoff)
+        {
+            for (int i = 0; i < gradients.Count; i++)
+            {
+                gradients[i] = new Vector3d();
+            }
+            for (int i = 0; i < connections.Count; i++)
+            {
+                SetGradients(i, cutoff);
+            }
+        }
+
+        public void ApplyGradient(double factor = 1)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (!lockedDOF[i])
+                    nodes[i] += gradients[i] * factor;
+            }
+        }
+
+        double EvaluateElement(int eIndex, double cutoff)
+        {
+            var a = nodes[connections[eIndex].Item1];
+            var b = nodes[connections[eIndex].Item2];
+
+            var vec = b - a;
+            double norm = vec.SquareLength;
+            double l = Math.Sqrt(norm);
+
+            if (Math.Abs(vec.X) > Math.Abs(vec.Y) && Math.Abs(vec.X) > Math.Abs(vec.Z))
+            {
+                vec.X = 0;
+            }
+            else if (Math.Abs(vec.Y) > Math.Abs(vec.Z))
+            {
+                vec.Y = 0;
+            }
+            else
+            {
+                vec.Z = 0;
+            }
+
+            return Math.Min(cutoff * cutoff * 0.4, vec.SquareLength / (norm));
+        }
+        void SetGradients(int eIndex, double f)
+        {
+            int i = connections[eIndex].Item1;
+            int j = connections[eIndex].Item2;
+            var a = nodes[i];
+            var b = nodes[j];
+
+            var vec = b - a;
+            double norm = vec.SquareLength;
+            double l = Math.Sqrt(norm);
+            double sqnorm = 1 / (norm * l);
+            Vector3d res = new Vector3d();
+            if (Math.Abs(vec.X) > Math.Abs(vec.Y) && Math.Abs(vec.X) > Math.Abs(vec.Z))
+            {
+                res.X = -2 * vec.X * (vec.Y * vec.Y + vec.Z * vec.Z) * sqnorm;
+                res.Y = 2 * vec.X * vec.X * vec.Y * sqnorm;
+                res.Z = 2 * vec.X * vec.X * vec.Z * sqnorm;
+            }
+            else if (Math.Abs(vec.Y) > Math.Abs(vec.Z))
+            {
+                res.X = 2 * vec.Y * vec.Y * vec.X * sqnorm;
+                res.Y = -2 * vec.Y * (vec.X * vec.X + vec.Z * vec.Z) * sqnorm;
+                res.Z = 2 * vec.Y * vec.Y * vec.Z * sqnorm;
+            }
+            else
+            {
+                res.X = 2 * vec.Z * vec.Z * vec.X * sqnorm;
+                res.Y = 2 * vec.Z * vec.Z * vec.Y * sqnorm;
+                res.Z = -2 * vec.Z * (vec.X * vec.X + vec.Y * vec.Y) * sqnorm;
+            }
+            res = SmoothConstrain(res, f) / (l * 2);
+            gradients[i] += res;
+            gradients[j] -= res;
+        }
+        Vector3d SmoothConstrain(Vector3d v, double f)
+        {
+            const double lower = 0.01;
+            const double fade = 0.15;
+            double lf = f - fade;
+            double uf = f + fade;
+            if (v.SquareLength < lf * lf)
+            {
+                return v;
+            }
+            else if (v.SquareLength < uf * uf)
+            {
+                double l = v.Length;
+                double t = (l - lf) / (2 * fade);
+                double newLength = (t * lower + (1 - t) * (f - fade));
+                v *= newLength / l;
+                return v;
+            }
+            else
+            {
+                v.Unitize();
+                v *= lower;
+                return v;
+            }
+        }
+    }
+}
